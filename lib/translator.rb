25ca91114da4094ff09fc2f48b41cf5f8d24d456
Jake Mills <jake.mills.05@gmail.com>
2020-07-07 20:46:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,6 +11,8 @@ def load_library(file_path)
 
   emoticons.each do |key, value|
     new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
     binding.pry
   end
 # binding.pry
