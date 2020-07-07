b13840b53ccb1afee082f60b8cf6f605e91ff29b
Jake Mills <jake.mills.05@gmail.com>
2020-07-07 21:48:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
 
-  library.each do |key, value|
+  library.find do |key, value|
     if [key][value] == emoticon
       library[key][value]
     else
