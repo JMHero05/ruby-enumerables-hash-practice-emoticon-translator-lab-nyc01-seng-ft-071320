3f1c2def3a938cd969c6aeca21673bc3ab6c0311
Jake Mills <jake.mills.05@gmail.com>
2020-07-07 20:23:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,12 +8,12 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 # binding.pry
   emoticons.each_with_object({}) do |(emotion, symbol), new_hash|
-    binding.pry
+    # binding.pry
     if !new_hash[emotion]
       new_hash[emotion] = {}
     end
     # emotion.each do |english_sym, japanese_sym|
-
+binding.pry
     end
   end
 end
