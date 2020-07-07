de8108e2417c4d28752dddfc3360976fbc280a91
Jake Mills <jake.mills.05@gmail.com>
2020-07-07 21:27:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,8 +27,7 @@ def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
 
   library.detect do |emoticon|
-    library[key][:japanese] == emoticon
-    binding.pry
+    library[key][:japanese] == emoticon binding.pry
   end
   # binding.pry
 end
